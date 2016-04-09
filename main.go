855bcfb2c16891fc2e1b2c8eefe2fd8883b39a0a
kiongf <kiongf@126.com>
2016-04-09 19:52:09 +0800

    解决拉取镜像后，无法更改镜像状态的问题 解决无法push镜像到私有仓库的问题 解决只会推送一个镜像的问题 //待解决- 随机测试一个镜像耗时过长

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ var (
 	log                = logrus.New()
 	HostJson           = "./host.json"
 	ImageJson          = "./image.json"
-	imageMap           = make(map[string]*Image)
+	imageMap           = make(map[string]Image)
 	iMap               = new(sync.RWMutex)
 	appSoarRegistry    string
 	appSoarPort        string
@@ -84,12 +84,13 @@ func pushImageExists(c2 Client) {
 	log.Debugf("%s:push image exists in local", c2.Ip)
 	list, err := c2.getImages()
 	if err != nil {
-		log.Errorf("cant not get image list")
+		log.Errorf("%s:can not get image list:%v", c2.Ip, err)
 		return
 	}
+	log.Debugf("%s image list:%v\n", c2.Ip, list)
 	//尝试推送当前已存在的镜像
 	for _, j := range list {
-		if i, exists := imageMap[j.Image]; exists {
+		if i, exists := imageMap[j.ImageTag]; exists {
 			if i.state == unpull {
 				log.Debugf("%s is unpulled", i.Name)
 				iMap.Lock()
@@ -98,21 +99,41 @@ func pushImageExists(c2 Client) {
 					iMap.Unlock()
 					continue
 				} else {
-					log.Debugf("%s:pushing local %s", c2.Ip, j.Image)
+					log.Debugf("%s:pushing local %s", c2.Ip, j.ImageTag)
 					i.state = pushing
+					imageMap[j.ImageTag] = i
 					iMap.Unlock()
-					err := c2.pushImage(j.Image)
-					iMap.Lock()
+
+					newrepo := appSoarRegistry + ":" + appSoarPort
+					imagePush := strings.Replace(j.ImageTag, "docker.io", newrepo, 1)
+					log.Debugf("%s:tag [%v==>%v] when pushing", c2.Ip, j.ImageTag, imagePush)
+					err := c2.tagImage(j.ImageTag, imagePush)
+					if err != nil {
+						log.Errorf("%s:tag image[%v ==> %v] fail:%v", c2.Ip, j.ImageTag, imagePush, err)
+						iMap.Lock()
+						i.state = unpull
+						imageMap[j.ImageTag] = i
+						iMap.Unlock()
+						continue
+					}
+
+					err = c2.pushImage(imagePush)
 					if err != nil {
 						log.Errorf("%s:fail to push local image to %s for %v:", c2.Ip, appSoarRegistry+":"+appSoarPort, err)
+						iMap.Lock()
 						i.state = unpull
+						imageMap[j.ImageTag] = i
+						iMap.Unlock()
 					} else {
+						iMap.Lock()
 						i.state = pushed
+						imageMap[j.ImageTag] = i
+						iMap.Unlock()
+						pMap.Lock()
+						pushedMap[imagePush] = 1
+						imageMap[j.ImageTag] = i
+						pMap.Unlock()
 					}
-					iMap.Unlock()
-					pMap.Lock()
-					pushedMap[j.Image] = 1
-					pMap.Unlock()
 				}
 			}
 		}
@@ -121,53 +142,73 @@ func pushImageExists(c2 Client) {
 
 func pushImageNotExists(c2 Client) {
 	log.Debugf("%s:push image not exists in local", c2.Ip)
-	for _, i := range imageMap {
+	for k, i := range imageMap {
 		if i.state == unpull {
-			log.Debugf("%s:try to push %s", c2.Ip, i.Name)
+			log.Debugf("%s:try to push %s ", c2.Ip, i.Name)
 			iMap.Lock()
 			if i.state != unpull {
 				iMap.Unlock()
 				continue
 			} else {
 				i.state = pulling
+				imageMap[k] = i
 				iMap.Unlock()
-				err := c2.pullImageFromPublic(i.Name + ":" + i.Tag)
-				log.Debugf("%s:pulling from remote %s", c2.Ip, i.Name+":"+i.Tag)
-				if err == nil {
-					log.Debugf("%s:pulling from  public success %s", c2.Ip, i.Name+":"+i.Tag)
-					slice := strings.SplitN(i.Name, "/", 2)
-					newrepo := appSoarRegistry + ":" + appSoarPort + "/" + slice[1]
 
-					log.Debugf("new repo %v:%v", newrepo, i.Tag)
-					err = c2.tagImage(i.Name+":"+i.Tag, newrepo+":"+i.Tag)
+				tmpImageTag := i.Name + ":" + i.Tag
+				err := c2.pullImageFromPublic(tmpImageTag)
+				log.Debugf("%s:pulling from remote %s", c2.Ip, tmpImageTag)
+				if err == nil {
+					log.Debugf("%s:pulling from  public success %s", c2.Ip, tmpImageTag)
+					imagePush := strings.Replace(tmpImageTag, "docker.io", appSoarRegistry+":"+appSoarPort, 1)
+
+					log.Debugf("image push to registry :[%s]", imagePush)
+					err = c2.tagImage(tmpImageTag, imagePush)
 					if err != nil {
-						log.Errorf("%s:tag image[%v ==> %v] fail:%v", c2.Ip, i.Name+":"+i.Tag, newrepo+":"+i.Tag, err)
+						log.Errorf("%s:tag image[%v ==> %v] fail:%v", c2.Ip, tmpImageTag, imagePush, err)
 						iMap.Lock()
 						i.state = unpull
+						imageMap[k] = i
 						iMap.Unlock()
-						return
+						continue
+					}
+					log.Debugf("%s:tag image[%v ==> %v] ok", c2.Ip, tmpImageTag, imagePush)
+
+					iMap.Lock()
+					if i.state == pushing || i.state == pushed {
+						iMap.Unlock()
+						continue
+					} else {
+						i.state = pushing
+						imageMap[k] = i
+						iMap.Unlock()
 					}
 
-					err = c2.pushImage(newrepo + ":" + i.Tag)
+					err = c2.pushImage(imagePush)
 					if err != nil {
 						log.Errorf("%s:push fail:%v", c2.Ip, err)
 						iMap.Lock()
 						i.state = unpull
+						imageMap[k] = i
 						iMap.Unlock()
 					} else {
-						log.Debugf("%s:pushing remote to registry %s", c2.Ip, i.Name+":"+i.Tag)
+						log.Debugf("%s:push remote to registry %s success", c2.Ip, i.Name+":"+i.Tag)
 						iMap.Lock()
-						i.state = pulled
+						log.Debugf("%s: set %s's state to 4:%p", c2.Ip, i.Name+":"+i.Tag, &i)
+						i.state = pushed
+						imageMap[k] = i
 						iMap.Unlock()
 						pMap.Lock()
-						pushedMap[newrepo+":"+i.Tag] = 1
+						pushedMap[imagePush] = 1
 						pMap.Unlock()
 					}
 
 				} else {
 					log.Errorf("%s:pulling from public fail:%v", c2.Ip, err)
 					iMap.Lock()
-					i.state = unpull
+					if i.state != pushing || i.state != pushed {
+						i.state = unpull
+						imageMap[k] = i
+					}
 					iMap.Unlock()
 					log.Error(err)
 				}
@@ -179,7 +220,7 @@ func pushImageNotExists(c2 Client) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	log.Debug("start to register ..............." + r.RemoteAddr)
+	log.Infof("%s:start to register ", r.RemoteAddr)
 	vars := mux.Vars(r)
 	port := vars["port"]
 	log.Debug(r.RemoteAddr)
@@ -205,11 +246,33 @@ func register(w http.ResponseWriter, r *http.Request) {
 	//否则客户端一直得不到回应
 	go func() {
 		log.Debug("client prepare registry's environment")
-		pushImageExists(c2)
-		//可能还有未推送的,且不在本地的镜像
-		pushImageNotExists(c2)
+		go func() {
+			for {
+				healthCheck(c2)
+				time.Sleep(1 * time.Second)
+			}
+		}()
+		for i, j := range imageMap {
+			log.Debugf("prepare: %v === %v\n", i, j)
+		}
+		for {
+			allPushed := true
+			pushImageExists(c2)
+			//可能还有未推送的,且不在本地的镜像
+			pushImageNotExists(c2)
+			for _, j := range imageMap {
+				log.Debugf("prepare env ========>image:%s,state:%d:j:%p\n", j.Name, j.state, &j)
+				if j.state != pushed {
+					allPushed = false
+				}
+			}
 
-		go healthCheck(c2)
+			if allPushed {
+				break
+			}
+			time.Sleep(2 * time.Second)
+		}
+
 		//开启10个goroutine进行随机测试
 		for i := 0; i < defaultRandNum; i++ {
 			go func(c Client) {
@@ -224,17 +287,19 @@ func register(w http.ResponseWriter, r *http.Request) {
 						randTest(c)
 					}
 					//for test
-					time.Sleep(2 * time.Second)
 				}
 			}(c2)
 		}
 	}()
+
+	msg := appSoarRegistry + ":" + appSoarPort
+	fmt.Fprintf(w, msg)
 	return
 }
 
 //解析客户端镜像列表
 type ClientImageList struct {
-	Image string `json:"image"`
+	ImageTag string `json:"image"`
 }
 
 func (c Client) getImages() ([]ClientImageList, error) {
@@ -260,7 +325,7 @@ func (c Client) getImages() ([]ClientImageList, error) {
 		return []ClientImageList{}, errors.New("json unmarshal apiimage fail")
 	}
 
-	return []ClientImageList{}, nil
+	return apiImages, nil
 }
 
 func (c Client) pullImage(imgTag string) error {
@@ -296,6 +361,7 @@ func (c Client) pullImage(imgTag string) error {
 }
 
 func (c Client) pushImage(imgTag string) error {
+	log.Debugf("%s pushImage try to push [%s]", c.Ip, imgTag)
 	slice1 := strings.Split(imgTag, ":")
 
 	var img string
@@ -534,6 +600,7 @@ func RandStringRunes(n int) string {
 
 func init() {
 
+	log.Level = logrus.DebugLevel
 	rand.Seed(time.Now().Unix())
 
 	//flag.StringVar(&appSoarRegistry, "rip", "192.168.15.119", "registry ip")
@@ -550,16 +617,12 @@ func init() {
 	}
 
 	log.Formatter = &logrus.TextFormatter{DisableColors: true}
-	fp, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644) // 让程序关闭后,自动释放
+	fp, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644) // 让程序关闭后,自动释放
 	if err != nil {
 		log.Errorf("Create logfile %s fail\n", logFile)
 		os.Exit(1)
 	}
 	log.Out = fp
-
-}
-
-func main() {
 
 	var ilist Imagelist
 	strImage, err := ioutil.ReadFile(ImageJson)
@@ -580,8 +643,20 @@ func main() {
 	}
 
 	for _, i := range ilist.Images {
-		imageMap[i.Name+":"+i.Tag] = &i
+		log.Debugf("ilist :%s", i)
+		if !strings.HasPrefix(i.Name, "docker.io/") {
+			i.Name = "docker.io/" + i.Name
+		}
+
+		imageMap[i.Name+":"+i.Tag] = i
 	}
+	for i, j := range imageMap {
+		log.Infof("%v:%v\n", i, j)
+	}
+
+}
+
+func main() {
 
 	//	registry healthy check
 	log.Debugf("start registry client, healthCheck\n")
